61a95c0808968235bfd6f775c1cb7cbfe1fdd7d0
Timur Abragimovich <timurkatime@gmail.com>
2022-08-06 19:28:37 +0200

    Coding style

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# caesar-cipher
+# Сaesar decipher
  
+<h3> Decoding: </h3>
+
+1. Create textfile with inputs: 1st line - coded message, 2nd line - overheard message
+2. Start main.py process by
+<pre><code> python3 main.py *name of created file* </code></pre>
+
+3. Open output.txt file. Here is decoded message.
+
+
+P.S. Сompleted as part of the homework for the subject "BAB37ZPR – Základy programování"
--- main.py
+++ main.py
@@ -1,72 +1,105 @@
+"""
+* BAB37ZPR - Základy programování
+* HW03 - Caesarova šifra
+* Uploaded at 12.11.2021 13:07
+* Score: 5/5
+* Coding style chaged at 6.8.2022
+"""
 import sys
 
-abcd = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-file = str(sys.argv[1])
-file = open(file,'r')
-lines = file.readlines()
-kod = lines[0]
-kod = kod[:(len(kod)-1)]
-podsl = lines[1]
-file.close()
+abcd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-def odsifr(kod,podsl):
-    p = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-    for k in range(0,52):
-        for j in range(len(kod)):
-            ind = abcd.index(kod[j])
-            if (abcd[(ind+k)%52] == podsl[j]):
-                p[k].append(1)
-    k = p.index((max(p)))
-    return k
 
-def result(k):
+def main():
+    code, overhear = get_code_overhear(sys.argv[1])
+    if not check_input(code, overhear):
+        return False
+
+    with open('output.txt', 'w') as output_fd:
+        if not output_fd:
+            print("Error: can't open output file!")
+            return False
+
+        result = shift_code(code, get_shift_amount(code, overhear))
+        output_fd.write(result)
+
+    return True
+
+
+def get_code_overhear(filename):
+    with open(filename, 'r') as fd:
+        lines = fd.readlines()
+        # get code msg
+        code = lines[0]
+        code = code[:(len(code)-1)]
+        # get overhear msg
+        overhear = lines[1]
+
+    return code, overhear
+
+
+def get_shift_amount(code, overhear):
+    match_counter = [[] for _ in range(len(abcd))]
+
+    for i in range(len(abcd)):
+        for j in range(len(code)):
+            idx = abcd.index(code[j])
+            if (abcd[(idx+i) % len(abcd)] == overhear[j]):
+                match_counter[i].append(1)
+
+    shift = match_counter.index((max(match_counter)))
+    return shift
+
+
+def shift_code(code, shift_amount):
     result = ''
-    for i in range(len(kod)):
-        ind = abcd.index(kod[i])
-        result = result + ((abcd[(ind+k)%52]))
+
+    for i in range(len(code)):
+        idx = abcd.index(code[i])
+        result = result + ((abcd[(idx + shift_amount) % len(abcd)]))
+
     return result
 
 
-def errors_check():
-    def chybny_vstup():
-        for i in range(len(kod)):
+def check_input(code, overhear):
+    def check_input_by_abcd():
+        for i in range(len(code)):
             try:
-                abcd.index(kod[i])
+                abcd.index(code[i])
             except IndexError:
                 pass
             except ValueError:
                 return False
-        for j in range(len(podsl)):
+        for j in range(len(overhear)):
             try:
-                abcd.index(podsl[j])
-            except ValueError: 
+                abcd.index(overhear[j])
+            except ValueError:
                 return False
             except IndexError:
                 pass
         return True
 
-    def delka_vstupu():
-        if (len(kod) > len(podsl)) or (len(kod) < len(podsl)):
+    def check_input_length():
+        if (len(code) > len(overhear)) or (len(code) < len(overhear)):
             return False
         return True
 
-    if chybny_vstup() == False:
-        print('Error: Chybny vstup!')
-        exit()
-    if chybny_vstup() == False and delka_vstupu() == False:
-        print('Error: Chybny vstup!')
-        exit()
-    if delka_vstupu() == False:
-        print('Error: Chybna delka vstupu!')
-        exit()
+    if not check_input_by_abcd():
+        print('Error: Wrong input!')
+        return False
+    if not check_input_by_abcd() and not check_input_length():
+        print('Error: Wrong input!')
+        return False
+    if not check_input_length():
+        print('Error: Wrong input length!')
+        return False
 
-def tisk(vystup):
-    file = open('output.txt','w')
-    if errors_check:
-        file.write(vystup)
-        file.close()
+    return True
 
-errors_check()
-tisk(result(odsifr(kod,podsl)))
+
+if __name__ == '__main__':
+    main()
